846ec6c1adf627f9149bc0c05b0436721fdcdf72
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2022-11-15 15:30:36 +0200

    Add .spaceshiprc.zsh

---
+++ full/.spaceshiprc.zsh
@@ -0,0 +1,14 @@
+SPACESHIP_TIME_SHOW=false 
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+SPACESHIP_DIR_TRUNC_REPO=true
+
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+spaceship add --before char vi_mode
+
+# kubectl
+SPACESHIP_KUBECTL_SHOW=true
+SPACESHIP_KUBECTL_VERSION_SHOW=false
+SPACESHIP_KUBECTL_SYMBOL=  
