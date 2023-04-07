a28c69c158f5cafc1fde1483ed7fc657f0646571
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2023-04-07 15:17:12 +0300

    Install software packages

--- install-essential.sh
+++ install-essential.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+echo "Installing git"
+sudo apt install git
+
+echo "Installing curl"
+sudo apt install curl
+
+echo "Installing tmux"
+sudo apt install tmux
+
+echo "Installing tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 dotfiles_dir=~/dot-files/essential
 
 echo "Creating assential symlinks..."
--- install-full.sh
+++ install-full.sh
@@ -2,6 +2,14 @@
 
 ./install-essential.sh
 
+echo "Installing zsh"
+sudo apt install zsh
+chsh -s $(which zsh)
+
+echo "Installing antigen"
+curl -L git.io/antigen > ~/antigen.zsh
+
+
 dotfiles_dir=~/dot-files/full
 
 echo "Creating full symlinks..."
