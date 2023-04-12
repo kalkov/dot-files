0c597e9c5a00f19a684e415884b5e0bd103c1b2d
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2023-04-12 15:10:28 +0300

    Install krew

--- install-full.sh
+++ install-full.sh
@@ -10,6 +10,18 @@ echo "Installing antigen"
 curl -L git.io/antigen > ~/antigen.zsh
 
 
+echo "Installing krew"
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
 dotfiles_dir=~/dot-files/full
 
 echo "Creating full symlinks..."
