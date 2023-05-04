3d5ecc387c2f5d756d7ab2ab4b226ac16fc3bd9c
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2023-05-04 11:15:01 +0300

    Install rvm

--- install-full.sh
+++ install-full.sh
@@ -22,6 +22,14 @@ echo "Installing krew"
   ./"${KREW}" install krew
 )
 
+
+echo "Installing RVM"
+gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt update
+sudo apt install rvm
+sudo usermod -a -G rvm $USER  
+
 dotfiles_dir=~/dot-files/full
 
 echo "Creating full symlinks..."
