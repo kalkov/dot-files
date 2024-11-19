feca8e6f678b72e93a9fd64b7c41662b24056424
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2024-11-19 11:41:29 +0200

    Install kubectx and kubens

--- install-full.sh
+++ install-full.sh
@@ -22,6 +22,11 @@ echo "Installing krew"
   ./"${KREW}" install krew
 )
 
+echo "Installing kubectx"
+kubectl krew install ctx
+
+echo "Installing kubens"
+kubectl krew install ns
 
 echo "Installing RVM"
 gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
